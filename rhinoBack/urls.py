a65fcc552ab37aef0c4e8dac7796640f5f5c300a
Adib Khan <110726624+adibkhandev@users.noreply.github.com>
2023-11-14 04:01:43 -0800

    debugging p1

--- rhinoBack/urls.py
+++ rhinoBack/urls.py
@@ -20,7 +20,8 @@ from rest_framework_simplejwt.views import (
 
 from django.contrib import admin
 from django.urls import path,include
-
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('rhinoApi.urls')),
@@ -28,4 +29,4 @@ urlpatterns = [
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     
 
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
